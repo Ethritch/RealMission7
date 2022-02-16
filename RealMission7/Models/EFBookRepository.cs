e567ec962cd738524e7f8ef98531781c324d19fb
Ethritch <ethritch@gmail.com>
2022-02-16 10:30:37 -0700

    Add project files.

---
+++ RealMission7/Models/EFBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealMission7.Models
+{
+    public class EFBookRepository : iBookRepository
+    {
+
+        private BookstoreContext context { get; set; }
+
+        public EFBookRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Books> Books => context.Books;
+    }
+}
